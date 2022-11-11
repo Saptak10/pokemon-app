a7af6f267f60e77137bcafee267fb5a4bf1b3bdf
Saptak10 <saptak.bugatti20@gmail.com>
2022-11-11 17:28:27 +0530

    changes

--- controller/pokemonController.js
+++ controller/pokemonController.js
@@ -1,10 +1,11 @@
 const asyncHandler = require('express-async-handler')
 // const Pokemon = require('../models/pokemonModel')
-// const pokemons = require('../data')
+const axios = require('axios');
+const pokemons = axios.get(`https://api.pokemontcg.io/v2/cards?page=1&pageSize=1`)
 
 module.exports.getPokemon = asyncHandler(async (req, res) => {
-    // res.json(pokemons)
-    res.json({ message: 'Hello' })
+    res.json(pokemons)
+    // res.json({ message: 'Hello' })
 })
 
 module.exports.getPokemonById = asyncHandler(async(req, res) => {
